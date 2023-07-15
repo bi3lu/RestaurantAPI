f86cbe17eb0c168d5054007c8dbd8bae266c62cc
Kuba Bielecki <kuba.bielecki@outlook.com>
2023-07-15 17:36:38 +0200

    Mapowanie zapytań do akcji w kontrolerze (Sekcja 1 - 12)
    Wykonano mapowanie zapytań dla polecenia GET oraz POST

--- RestaurantAPI/Controllers/WeatherForecastController.cs
+++ RestaurantAPI/Controllers/WeatherForecastController.cs
@@ -22,5 +22,19 @@ namespace RestaurantAPI.Controllers
 
             return result;
         }
+
+        [HttpGet("currentDay/{max}")]
+        public IEnumerable<WeatherForecast> Get2([FromQuery]int take, [FromRoute]int max)
+        {
+            var result = _service.Get();
+
+            return result;
+        }
+
+        [HttpPost]
+        public ActionResult<string> Hello([FromBody]string name)
+        {
+            return Ok($"Hello {name}");
+        }
     }
 }
