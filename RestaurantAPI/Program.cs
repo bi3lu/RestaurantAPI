dcd6ec2ada8237653e3add0bd7f810d794fd3240
Kuba Bielecki <kuba.bielecki@outlook.com>
2023-07-16 10:30:04 +0200

    Dodanie Entity Framework
    Dodanie paczki EF do projektu, utworzenie bazy danych oraz zasilenie początkowymi danymi

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,3 +1,5 @@
+using RestaurantAPI.Entities;
+
 namespace RestaurantAPI
 {
     public class Program
@@ -10,10 +12,15 @@ namespace RestaurantAPI
 
             builder.Services.AddTransient<IWeatherForecastService, WeatherForecastService>();
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<RestaurantDbContext>();
+            builder.Services.AddScoped<RestaurantSeeder>();
 
             var app = builder.Build();
+            var scope = app.Services.CreateScope();
+            var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
 
             // Configure the HTTP request pipeline.
+            seeder.Seed();
 
             app.UseHttpsRedirection();
 
